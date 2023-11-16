26145673235fb2446d837c35431d678a64e4fd7b
rootspyro <root.spyro@gmail.com>
2023-11-16 19:19:31 -0400

    db connection made

--- backend/router/health.go
+++ backend/router/health.go
@@ -2,26 +2,47 @@
 
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"50thbeers/db"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 type HealthRouter struct {
    group *gin.RouterGroup
+   db    *db.DB
 }
 
-func NewHealthRouter( g *gin.RouterGroup ) *HealthRouter {
+func NewHealthRouter( g *gin.RouterGroup, db *db.DB ) *HealthRouter {
    return &HealthRouter{
       group: g,
+      db: db,
    }
 }
 
 func ( hr *HealthRouter ) Setup() {
   
    hr.group.GET("/health", func(ctx *gin.Context) {
+
+      err := hr.db.Conn.Ping()
+
+      if err != nil {
+
+         log.Printf("Database PING error: %v", err)
+
+         ctx.JSON(500, gin.H{
+            "success": false,
+            "data": "Something went wrong!",
+         })
+
+         return
+      }
       
       ctx.JSON(200, gin.H{
          "success": true,
          "data": "Server is running!",
       })
-   });
+   })
 }
 
--- backend/router/setup.go
+++ backend/router/setup.go
@@ -1,14 +1,20 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"50thbeers/db"
+
+	"github.com/gin-gonic/gin"
+)
 
 type SetupRouter struct {
    server *gin.Engine
+   db     *db.DB
 }
 
-func NewSetupRouter(s *gin.Engine) *SetupRouter {
+func NewSetupRouter(s *gin.Engine, db *db.DB) *SetupRouter {
    return &SetupRouter{
       server: s,
+      db: db,
    }
 }
 
@@ -17,9 +23,9 @@ func( sr *SetupRouter ) Setup() {
    v1 := sr.server.Group("v1");
 
    // PATHS SETUP
-   healthPath := NewHealthRouter(v1);
+   healthPath := NewHealthRouter(v1, sr.db);
 
-   healthPath.Setup();
+   healthPath.Setup()
 
    // NOT FOUND
    sr.server.NoRoute(func(ctx *gin.Context) {
@@ -28,5 +34,5 @@ func( sr *SetupRouter ) Setup() {
          "status": false,
          "data": "404 Page not found",
       })
-   });
+   })
 }
