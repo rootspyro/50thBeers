f1204bebdb9ff2d8a3a8235dc07edc9b8763e2b9
rootspyro <root.spyro@gmail.com>
2023-12-01 20:30:16 -0400

    Feature: Drinks Structs created

---
+++ backend/models/drinks.go
@@ -0,0 +1,57 @@
+package models
+
+type Drink struct {
+  DrinkId      string  `json:"drinkId"`
+  DrinkName    string  `json:"drinkName"`
+  DrinkType    string  `json:"drinkType"`
+  CountryName  string  `json:"countryName"`
+  TastingDate  string  `json:"tastingDate"`
+  ABV          float32 `json:"abv"`
+  Rating       int     `json:"rating"`
+  PictureUrl   string  `json:"pictureUrl"`
+  LocationName string  `json:"locationName"`
+  Appearance   string  `json:"appearance"`
+  Aroma        string  `json:"aroma"`
+  Taste        string  `json:"taste"`
+  Comments     string  `json:"comments"`
+  CreatedAt    string  `json:"createdAt"`
+  PublicatedAt string  `json:"publicatedAt"`
+  UpdatedAt    string  `json:"updatedAt"`
+  Status       string  `json:"status"`
+}
+
+type DrinkCollection struct {
+  ItemsFound int             `json:"itemsFound"`
+  Items      []DrinkGeneral  `json:"items"`
+  Filters    []Filter        `json:"avaiableFilters"`
+}
+
+type DrinkGeneral struct {
+  DrinkId      string  `json:"drinkId"`
+  DrinkName    string  `json:"drinkName"`
+  DrinkType    string  `json:"drinkType"`
+  CountryName  string  `json:"countryName"`
+  TastingDate  string  `json:"tastingDate"`
+  ABV          float32 `json:"abv"`
+  Rating       int     `json:"rating"`
+  PictureUrl   string  `json:"pictureUrl"`
+  CreatedAt    string  `json:"createdAt"`
+  PublicatedAt string  `json:"publicatedAt"`
+  UpdatedAt    string  `json:"updatedAt"`
+  Status       string  `json:"status"`
+}
+
+type DrinkBody struct {
+  DrinkName    string  `json:"drinkName"`
+  DrinkType    string  `json:"drinkType"`
+  CountryId    int     `json:"countryId"`
+  TastingDate  string  `json:"tastingDate"`
+  ABV          float32 `json:"abv"`
+  Rating       int     `json:"rating"`
+  PictureUrl   string  `json:"pictureUrl"`
+  LocationId   int     `json:"locationId"`
+  Appearance   string  `json:"appearance"`
+  Aroma        string  `json:"aroma"`
+  Taste        string  `json:"taste"`
+  Comments     string  `json:"comments"`
+}
--- backend/models/locations.go
+++ backend/models/locations.go
@@ -14,7 +14,7 @@ type Location struct {
 type LocationsCollection struct {
    ItemsFound int        `json:"itemsFound"`
    Items      []Location `json:"items"`
-   Filters    []Filter   `josn:"avaiableFilters"`
+   Filters    []Filter   `json:"avaiableFilters"`
 }
 
 type LocationBody struct {
