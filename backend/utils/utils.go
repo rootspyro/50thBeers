1001c4334abab833b0ccbf1fd09bb943c4696701
rootspyro <root.spyro@gmail.com>
2023-12-23 14:19:08 +0100

    Refactor: Add Country Handlers refactor

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -1,6 +1,12 @@
 package utils
 
-import "strings"
+import (
+	"50thbeers/models"
+	"log"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func NameToId( name string ) string {
 
@@ -8,4 +14,13 @@ func NameToId( name string ) string {
   name = strings.ReplaceAll(name, " ", "_")
 
   return name
+}
+
+// this function sends the json status and response for a server error
+// and prints in log the error
+func ServerError( ctx *gin.Context, err error ) {
+  
+  log.Println(err.Error())
+  models.ServerError(ctx)
+
 }
